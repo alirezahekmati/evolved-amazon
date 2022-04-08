fe74520a60ed92c19312099c07110cdd715a527d
alireza <alirezahekmati80@gmail.com>
2022-04-08 13:24:14 -0700

    first commit

---
+++ src/Component/AlirezaHello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './AlirezaHello.css'
+function AlirezaHello() {
+    return (
+        <div className='AlirezaHello'>
+            hello !
+        </div>
+    )
+}
+
+export default AlirezaHello
